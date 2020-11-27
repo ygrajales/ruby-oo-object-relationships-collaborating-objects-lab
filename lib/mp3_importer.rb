778c4887db6e2cb1dcc670996e3e1b85048bc7fc
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-11-27 15:55:00 -0600

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+
+    attr_reader :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        @files || Dir.glob("#{path}/*.mp3").collect{|x| x.gsub("#{path}/", "")}
+    end
+
+    def import 
+        files.each{|x| Song.new_by_filename(x)}
+    end
+
+end
